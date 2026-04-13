b0fe510ddc6e7864f7c55009be95ac200ef92b06
Gatho <61396959+theob55@users.noreply.github.com>
2026-04-13 21:37:40 +0200

    clusters+BDPT and OMP
    Merging tree_clusters with BDPT and making OpenMP functional again

--- includes/light.h
+++ includes/light.h
@@ -20,10 +20,9 @@ void benchmark_BDPT(Scene* scene, size_t width, size_t height);
  * @param dmax maximum bounces number
  * @param radiance color of the pixels at the e object
  */
-void ray_sampling(Ray * r, const Scene * S, int dmax, Vector * radiance, unsigned int* seed);
 void ray_sampling_t(Ray* const r, object_tree_t* const scene, int dmax, Vertex * path, unsigned int* seed, int * path_length, Vector* bg_color);
 
-void compute_vertex(Vector * color, Vertex *camera_path, int camera_path_length, Vertex *light_path, int light_path_length, Scene const * S);
+void compute_vertex(Vector * color, Vertex *camera_path, int camera_path_length, Vertex *light_path, int light_path_length, Large_BVH_t* const tree);
 
 /**
  * @brief Trace 3D ray from the camera with Path Tracing with N samples
@@ -33,11 +32,7 @@ void compute_vertex(Vector * color, Vertex *camera_path, int camera_path_length,
  * @param N input: number of samples
  * @param color output: the color of the pixel
  */
-void path_trace(const int x1, const int y1, const int local_y, const int width, Scene const * S, const size_t bounces, float* color_buffer, unsigned int* seed);
-
-void path_trace_t(const int x1, const int y1, const int local_y, const int width, Scene const * S, const size_t bounces, float* color_buffer, unsigned int* seed, object_tree_t* const tree);
-void path_trace_clusters(const int x1, const int y1, const int local_y, const int width, Scene const * S, const size_t bounces, float* color_buffer, unsigned int* seed, Large_BVH_t* const tree);
-
+void path_trace_t(const int x1, const int y1, Scene const * S, const size_t bounces, Vector * pixel_color, unsigned int* seed, Large_BVH_t* const tree);
 
 int get_bounces(void);
 
