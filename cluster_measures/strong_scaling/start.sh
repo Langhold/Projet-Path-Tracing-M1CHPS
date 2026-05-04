30ad5139c1b9012473afceba563a6df626fac2a7
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 00:02:29 +0200

    Update start.sh

--- cluster_measures/strong_scaling/start.sh
+++ cluster_measures/strong_scaling/start.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=strong_scaling_ppn
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
-#SBATCH --time=00:05:00
+
 echo "Job started on $(hostname) at $(date)"
-
-module load gcc/13.2.0
-
-cmake -B build -DCMAKE_BUILD_TYPE=Release
-
-cmake --build build
 
 npc=1
 
 for J in {1..8}; do
 
 echo "Running with $npc CPUs..."
-sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/cluster.txt"
-sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/simd.txt"
-sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/naive.txt"
+sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/run_cluster.sh"
+#sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/simd.txt"
+#sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/naive.txt"
 
 npc=$((npc * 2))
 done
