3f6d361da8060876f98a6082b698dfd0e3e77c72
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 00:35:33 +0200

    Update start.sh

--- cluster_measures/strong_scaling/start.sh
+++ cluster_measures/strong_scaling/start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+#SBATCH --nodes=1
 
 echo "Job started on $(hostname) at $(date)"
 
-npc=1
+export npc=1
 
 for J in {1..8}; do
 
 echo "Running with $npc CPUs..."
-sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/run_cluster.sh"
-#sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/simd.txt"
-#sbatch --cpus-per-task=$npc ./build/ppm "cluster_measures/strong_scaling/huge/config/naive.txt"
+
+sbatch --cpus-per-task=$npc ./cluster_measures/strong_scaling/cluster_simd.sh
+sbatch --cpus-per-task=$npc ./cluster_measures/strong_scaling/run_simd.sh
+sbatch --cpus-per-task=$npc ./cluster_measures/strong_scaling/run_naive.sh
 
 npc=$((npc * 2))
 done
