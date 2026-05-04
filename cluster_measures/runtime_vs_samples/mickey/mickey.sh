f66d369f12be14090e1b801807a230b6b47d05fd
Gatho <61396959+theob55@users.noreply.github.com>
2026-05-04 12:00:07 -0500

    russian.txt

--- cluster_measures/runtime_vs_samples/mickey/mickey.sh
+++ cluster_measures/runtime_vs_samples/mickey/mickey.sh
@@ -2,7 +2,7 @@
 
 
 
-for J in naive.txt tree.txt cluster.txt simd.txt bdpt.txt; do
+for J in naive.txt tree.txt cluster.txt simd.txt bdpt.txt russian.txt; do
 
 mpirun -np 10 ./build/ppm "cluster_measures/runtime_vs_samples/mickey/config/${J}"
 
