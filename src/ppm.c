eeaf9b343be7b99d3838228d2526f586a64b7c46
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-04 18:38:02 +0200

    Update ppm.c

--- src/ppm.c
+++ src/ppm.c
@@ -166,7 +166,7 @@ void compute_naive(pt_config_t *config)
 						for (size_t i = 0; i < print_rate; ++i)
 						{
 
-							path_trace(x1, y1, local_y, width, &scene, bounces, local_color_buffer, &seed_per_threads);
+							path_trace(x1, y1, local_y, width, &scene, bounces, local_color_buffer, &seed_per_threads, 0);
 						}
 
 						local_color_buffer[(local_y * width + x1) * 3 + 0] += pixel_color.Data[0];
@@ -236,6 +236,144 @@ void compute_naive(pt_config_t *config)
 	free_scene_objects(&scene);
 }
 
+/* ============================================================================
+ *  compute_russian
+ * ========================================================================== */
+
+void compute_russian(pt_config_t *config)
+{
+	unsigned int seed = (unsigned int)time(NULL);
+
+	const int width = config->width;
+	const int height = config->height;
+	const size_t smpls = config->samples;
+	const int bounces = config->bounces;
+	const int measures = config->n_measures ? config->n_measures : 1;
+
+	int can_print_image = config->can_print;
+	size_t print_rate = config->print_rate ? config->print_rate : 1;
+
+	struct timespec t0, t1;
+
+	if (mpi_rank == 0)
+	{
+		print_config(config);
+	}
+
+	Scene scene;
+	config->benchmark(&scene, width, height);
+	Image_32bit *image = create_image_32bit(width, height);
+
+	const int per_t_height = config->height / mpi_size;
+	float *local_color_buffer = malloc(width * per_t_height * 3 * sizeof(float));
+	uint32_t *local_pixels_buffer = calloc(width * per_t_height, sizeof(uint32_t));
+
+	int start = per_t_height * mpi_rank;
+	int end = per_t_height * (mpi_rank + 1);
+
+	for (int m = 0; m < measures; ++m)
+	{
+		if (mpi_rank == 0)
+		{
+			printf("%d measure on %d\n", m + 1, measures);
+			clock_gettime(CLOCK_MONOTONIC, &t0);
+		}
+
+#pragma omp parallel
+		{
+#pragma omp for schedule(static)
+			for (int i = 0; i < width * per_t_height * 3; ++i)
+				local_color_buffer[i] = 0.0f;
+
+			Vector pixel_color;
+			unsigned int seed_per_threads = time(NULL) ^ getpid() ^ omp_get_thread_num();
+
+			for (size_t p = print_rate; p <= smpls; p += print_rate)
+			{
+
+#pragma omp for schedule(static)
+				for (int y1 = start; y1 < end; ++y1)
+				{
+					int local_y = y1 - (per_t_height * mpi_rank);
+
+					for (int x1 = 0; x1 < width; ++x1)
+					{
+						pixel_color.Data[0] = 0.0f;
+						pixel_color.Data[1] = 0.0f;
+						pixel_color.Data[2] = 0.0f;
+
+						for (size_t i = 0; i < print_rate; ++i)
+						{
+
+							path_trace(x1, y1, local_y, width, &scene, bounces, local_color_buffer, &seed_per_threads, 1);
+						}
+
+						local_color_buffer[(local_y * width + x1) * 3 + 0] += pixel_color.Data[0];
+						local_color_buffer[(local_y * width + x1) * 3 + 1] += pixel_color.Data[1];
+						local_color_buffer[(local_y * width + x1) * 3 + 2] += pixel_color.Data[2];
+					}
+				}
+
+#pragma omp single
+				{
+					if (mpi_rank == 0)
+						clock_gettime(CLOCK_MONOTONIC, &t1);
+					float inv_samples = 255.f / (float)p;
+
+					for (int y = 0; y < per_t_height; ++y)
+					{
+						for (int x = 0; x < width; ++x)
+						{
+							int idx = y * width + x;
+							int idx_rgb = idx * 3;
+							color_float_to_int(local_color_buffer, idx_rgb,
+											   local_pixels_buffer, idx, inv_samples);
+						}
+					}
+
+					if (mpi_size != 1)
+					{
+						if (mpi_rank == 0)
+						{
+							print_time(&t0, &t1, p, config, mpi_size);
+							if (can_print_image || p == smpls)
+							{
+								MPI_Gather(local_pixels_buffer, width * per_t_height, MPI_INT32_T,
+										   image->buffer, width * per_t_height, MPI_INT32_T,
+										   0, MPI_COMM_WORLD);
+								write_image_file_32bit(image, p, m, config->output_filename);
+								printf("image_32bit%zu written\n", p);
+							}
+							clock_gettime(CLOCK_MONOTONIC, &t0);
+						}
+						else
+						{
+							if (can_print_image || p == smpls)
+							{
+								MPI_Gather(local_pixels_buffer, width * per_t_height, MPI_INT32_T,
+										   NULL, width * per_t_height, MPI_INT32_T,
+										   0, MPI_COMM_WORLD);
+							}
+						}
+					}
+					else
+					{
+						print_time(&t0, &t1, 0, config, mpi_size);
+						memcpy(image->buffer, local_pixels_buffer,
+							   width * per_t_height * sizeof(uint32_t));
+						write_image_file_32bit(image, p, m, config->output_filename);
+						clock_gettime(CLOCK_MONOTONIC, &t0);
+					}
+				}
+			}
+		}
+	}
+
+	free(local_pixels_buffer);
+	free(local_color_buffer);
+	free_image_32bit(image);
+	free_scene_objects(&scene);
+}
 /* ============================================================================
  *  compute_simd
  * ========================================================================== */
@@ -844,6 +982,10 @@ int main(int argc, char **argv)
 
 	switch (config.implem)
 	{
+			
+	case russian:
+		compute_russian(&config);
+		break;
 	case naive:
 		compute_naive(&config);
 		break;
