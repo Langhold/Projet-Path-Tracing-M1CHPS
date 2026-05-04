0ac1b5ca524d29795fa7d9e0b9b13f9b9b89063c
Gatho <61396959+theob55@users.noreply.github.com>
2026-05-04 11:59:33 -0500

    russian.txt

--- cluster_measures/runtime_vs_samples/huge/huge.sh
+++ cluster_measures/runtime_vs_samples/huge/huge.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-for J in naive.txt tree.txt cluster.txt simd.txt bdpt.txt; do
+for J in naive.txt tree.txt cluster.txt simd.txt bdpt.txt russian.txt; do
 
 mpirun -np 10 ./build/ppm "cluster_measures/runtime_vs_samples/huge/config/${J}"
 
