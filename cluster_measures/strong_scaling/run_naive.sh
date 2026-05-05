fe13dfe9bc3c706adcd6a71114e27325ce3ba179
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 10:11:06 +0200

    Update run_naive.sh

--- cluster_measures/strong_scaling/run_naive.sh
+++ cluster_measures/strong_scaling/run_naive.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=strong_scaling_ppn
 #SBATCH --output=cluster_measures/strong_scaling/output/naive/naive_slurm-%j.out
 #SBATCH --error=cluster_measures/strong_scaling/output/naive/naive_slurm-%j.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --cpus-per-nodes=1
 
 echo "Running with $npc CPUs..."
