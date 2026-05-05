0dbc34def0aceee7f32f422735ceec462729c698
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 10:09:44 +0200

    Update start.sh

--- cluster_measures/strong_scaling/start.sh
+++ cluster_measures/strong_scaling/start.sh
@@ -8,9 +8,9 @@ export npc=1
 for J in {1..8}; do
 
 
-sbatch --wait --tasks-per-node=$npc ./cluster_measures/strong_scaling/cluster_simd.sh
-sbatch --wait --tasks-per-node=$npc ./cluster_measures/strong_scaling/run_simd.sh
-sbatch --wait --tasks-per-node=$npc ./cluster_measures/strong_scaling/run_naive.sh
+sbatch --wait --ntasks-per-node $npc ./cluster_measures/strong_scaling/cluster_simd.sh
+sbatch --wait --ntasks-per-node $npc ./cluster_measures/strong_scaling/run_simd.sh
+sbatch --wait --ntasks-per-node $npc ./cluster_measures/strong_scaling/run_naive.sh
 
 npc=$((npc * 2))
 done
