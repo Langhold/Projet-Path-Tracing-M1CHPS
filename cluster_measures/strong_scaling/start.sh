bb33253197775cbc712678d2081d499ce17cec82
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 10:12:59 +0200

    Update start.sh

--- cluster_measures/strong_scaling/start.sh
+++ cluster_measures/strong_scaling/start.sh
@@ -5,13 +5,13 @@ echo "Job started on $(hostname) at $(date)"
 
 export npc=1
 
-for J in {1..8}; do
+for J in {1 2 3 4 8 10 16 20 24 32}; do
 
+npc=$J
 
 sbatch --wait --ntasks-per-node $npc ./cluster_measures/strong_scaling/cluster_simd.sh
 sbatch --wait --ntasks-per-node $npc ./cluster_measures/strong_scaling/run_simd.sh
 sbatch --wait --ntasks-per-node $npc ./cluster_measures/strong_scaling/run_naive.sh
 
-npc=$((npc * 2))
 done
 
