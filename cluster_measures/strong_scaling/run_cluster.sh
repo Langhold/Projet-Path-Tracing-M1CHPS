8b09c4a1a60e24639645b9421f9fe24a666bdd7c
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 10:10:54 +0200

    Update run_cluster.sh

--- cluster_measures/strong_scaling/run_cluster.sh
+++ cluster_measures/strong_scaling/run_cluster.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=strong_scaling_ppn
 #SBATCH --output=cluster_measures/strong_scaling/cluster_slurm-%j_cluster.out
 #SBATCH --error=cluster_measures/strong_scaling/cluster_slurm-%j.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --cpus-per-task=1
 
 echo "Running with $npc CPUs..."
