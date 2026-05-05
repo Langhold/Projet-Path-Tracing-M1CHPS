f474faaf8e7b7779acc0430b0306f3cb522de221
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 10:07:01 +0200

    Update start.sh

--- cluster_measures/strong_scaling/start.sh
+++ cluster_measures/strong_scaling/start.sh
@@ -7,11 +7,10 @@ export npc=1
 
 for J in {1..8}; do
 
-echo "Running with $npc CPUs..."
 
-sbatch --cpus-per-task=$npc ./cluster_measures/strong_scaling/cluster_simd.sh
-sbatch --cpus-per-task=$npc ./cluster_measures/strong_scaling/run_simd.sh
-sbatch --cpus-per-task=$npc ./cluster_measures/strong_scaling/run_naive.sh
+sbatch --wait --tasks-per-node=$npc ./cluster_measures/strong_scaling/cluster_simd.sh
+sbatch --wait --tasks-per-node=$npc ./cluster_measures/strong_scaling/run_simd.sh
+sbatch --wait --tasks-per-node=$npc ./cluster_measures/strong_scaling/run_naive.sh
 
 npc=$((npc * 2))
 done
