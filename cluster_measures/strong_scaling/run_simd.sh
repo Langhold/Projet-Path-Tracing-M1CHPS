a91323770f4f131beee4cbf371f987118d107e4c
Nolwen Dolléans <126026345+nolwen-dolleans@users.noreply.github.com>
2026-05-05 10:11:21 +0200

    Update run_simd.sh

--- cluster_measures/strong_scaling/run_simd.sh
+++ cluster_measures/strong_scaling/run_simd.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=strong_scaling_ppn
 #SBATCH --output=cluster_measures/strong_scaling/output/naive/simd_slurm-%j.out
 #SBATCH --error=cluster_measures/strong_scaling/output/naive/simd_slurm-%j.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 
